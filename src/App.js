01e1cf881ff1a13d6b6e55870681cde9d9a94098
Isaac <isaac1180@aol.com>
2020-11-16 12:52:26 -0500

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route, withRouter, Redirect} from 'react-router-dom'
 
 class App extends React.Component {
 
-//   fetch("https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "6d374b39c8mshe47d179b0c5da0bp14c2bdjsna684b91be557",
-// 		"x-rapidapi-host": "nutritionix-api.p.rapidapi.com"
-// 	}
-// })
-// .then((r) => r.json())
-// .then(data => data)
-// .catch(err => {
-// 	console.error(err);
-// });
 
 state = {
   items: []
