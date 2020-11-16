a15089f7792fc56972019820c0786b2bf9501d9f
Isaac <isaac1180@aol.com>
2020-11-16 12:05:44 -0500

    added api

--- src/App.js
+++ src/App.js
@@ -15,6 +15,20 @@ class App extends React.Component {
 // .catch(err => {
 // 	console.error(err);
 // });
+
+
+  componentDidMount() {
+    fetch("https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat", {
+      	"method": "GET",
+      	"headers": {
+      		"x-rapidapi-key": "6d374b39c8mshe47d179b0c5da0bp14c2bdjsna684b91be557",
+      		"x-rapidapi-host": "nutritionix-api.p.rapidapi.com"
+      	}
+      }).then(r => r.json())
+      .then(console.log)
+    }
+  
+
  render(){
   return (
        <div className="App">
