e78f89ff1f1bb98628813bd2e7fe3a5d173a623c
Isaac <isaac1180@aol.com>
2020-11-16 12:01:34 -0500

    added import for react and changed to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+//   fetch("https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "6d374b39c8mshe47d179b0c5da0bp14c2bdjsna684b91be557",
+// 		"x-rapidapi-host": "nutritionix-api.p.rapidapi.com"
+// 	}
+// })
+// .then((r) => r.json())
+// .then(data => data)
+// .catch(err => {
+// 	console.error(err);
+// });
+ render(){
   return (
-    <div className="App">
-      
+       <div className="App">
+       <h1> Welcome to the macro management app  </h1>
     </div>
   );
+ }
+
+
+
+
+
+
 }
+ 
 
 export default App;
